7dd4647a61ee8569fcc31c83a46beab6a15912df
Jokha Al Harthy <jokha.hamed.official@gmail.com>
2025-11-26 20:11:17 +0400

    Adding the main file that starts the app and initialize Firebase

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:wajeeh/app_theme.dart';
+import 'firebase_options.dart';
+import 'providers/auth_provider.dart';
+import 'pages/login_page.dart';
+import 'pages/register_page.dart';
+import 'pages/home_page.dart';
+import 'pages/forgot_password_page.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
+import 'package:provider/provider.dart';
+import 'pages/WelcomeScreen.dart';
+import 'pages/OnboardingScreen.dart';
+import 'pages/OnboardingScreenTwo.dart';
+import 'pages/OnboardingScreenThree.dart';
+import 'pages/TermsPage.dart';
+import 'pages/PrivacyPolicyPage.dart';
+import 'pages/OtpVerificationPage.dart';
+import 'pages/Location_page.dart';
+import 'pages/Language_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => AuthProvider())],
+      child: MaterialApp(
+        theme: AppTheme.light,
+        debugShowCheckedModeBanner: false,
+        home: const OnboardingScreen(),
+        routes: {
+          "/register": (_) => const RegisterPage(),
+          "/login": (_) => const LoginPage(),
+          "/home": (_) => const HomePage(),
+          "/forgot": (_) => const ForgotPasswordPage(),
+          "/welcome": (_) => const WelcomePage(),
+          "/onboarding2": (_) => const OnboardingScreenTwo(),
+          "/onboarding3": (_) => const OnboardingScreenThree(),
+          "/privacy": (_) => const PrivacyPolicyPage(),
+          "/terms": (_) => const TermsPage(),
+          "/language":(_)=> const LanguageSelectionPage(),
+          "/location": (_) => const LocationSelectionPage(),
+          "/otp": (_) => const OtpVerificationPage(),
+        },
+      ),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+        if (snapshot.hasData) {
+          return const HomePage();
+        }
+        return const LoginPage();
+      },
+    );
+  }
+}
